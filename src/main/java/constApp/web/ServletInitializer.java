ebdc9f0bb7d2312d7c097ecdc4858086eb9cd299
Maximiliano Castro <maaxi.castro@gmail.com>
2022-10-06 11:38:05 -0300

    V 1.1 - arreglos en OC(agregar un 0 "nada", ) y en gastos (quitar tipo y nro de comprobante, agregar tipo de cambio, agregar pendiente de retiro)

--- src/main/java/constApp/web/ServletInitializer.java
+++ src/main/java/constApp/web/ServletInitializer.java
@@ -2,27 +2,12 @@ package constApp.web;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
-import java.util.concurrent.TimeUnit;
-
-@EnableWebMvc
-public class ServletInitializer extends SpringBootServletInitializer implements WebMvcConfigurer {
+public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(WebApplication.class);
-	}
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**")
-				.addResourceLocations("/css/")
-				.addResourceLocations("/js/")
-				.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
 	}
 
 }
